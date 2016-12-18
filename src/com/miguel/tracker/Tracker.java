65c9a0d15e9a4c8b34a90bfef6f63712c449fe28
Miguel <Miguel@DESKTOP-876ELT2>
2016-12-17 20:08:07 -0500

    adding comment

--- src/com/miguel/tracker/Tracker.java
+++ src/com/miguel/tracker/Tracker.java
@@ -5,19 +5,23 @@ import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.MoveController;
 
+/*
+ * This first example only moves robot forward with speed
+ * and rotates medium motor
+ */
+
 public class Tracker {
 
 	public static void main(String[] args) {
 
 		RegulatedMotor motor = Motor.A;
 		motor.rotate(1800, true);
-		
+
 		MoveController pilot = new DifferentialPilot(2.25f, 5.5f, Motor.C,
 				Motor.B);
 		pilot.setTravelSpeed(20d);
-		pilot.travel(-20);
-		
-		
+		pilot.travel(50);
+
 	}
 
 }
